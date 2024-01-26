1d9d5ecdb636c7d2d461344574ee845a961e799e
kvrameshqa <kvramesh.qa@gmail.com>
2024-01-26 20:22:34 +0530

    Create new testgitpro1

---
+++ src/test/java/testgitpro1/welcomtest.java
@@ -0,0 +1,12 @@
+package testgitpro1;
+
+public class welcomtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome");
+
+	}
+
+}
